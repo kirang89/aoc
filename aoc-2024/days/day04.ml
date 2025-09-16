0545984fc1f58bcada2c2fe5ac071d0adc3045fe
Kiran Gangadharan <me@kirang.in>
2025-09-16 20:12:56 +0530

    Refactor Day4 part 1

--- aoc-2024/days/day04.ml
+++ aoc-2024/days/day04.ml
@@ -1,157 +1,55 @@
-let fetch_grid_elem grid row col = String.get grid.(row) col
+let directions =
+  [ (0, 1); (1, 0); (1, 1); (0, -1); (-1, 0); (-1, -1); (1, -1); (-1, 1) ]
 
-let reverse_string string =
-  let len = String.length string in
-  String.init len (fun i -> String.get string (len - 1 - i))
-
-let search_occurrence_forward search_string row idx =
-  let length = String.length search_string in
-  if idx + (length - 1) >= String.length row then false
-  else if String.sub row idx length = search_string then
-    (* Printf.printf "forward search in row %s at pos %d\n" row idx; *)
-    true
-  else false
-
-let search_occurrence_backward search_string row idx =
-  let length = String.length search_string in
-  let string_rev = reverse_string search_string in
-  if idx - (length - 1) < 0 then false
-  else if String.sub row (idx - (length - 1)) length = string_rev then true
-  else false
-
-let search_occurrence_upward search_string grid row idx =
-  (* TODO: find_index won't work if there are duplicate lines *)
-  let row_idx = grid |> Array.find_index (fun l -> l = row) |> Option.get in
-  let search_length = String.length search_string - 1 in
-  let search_idxs = List.init (String.length search_string) (fun i -> i) in
-  if row_idx - search_length < 0 then false
+let grid_get_opt grid row col =
+  if row < 0 || row >= Array.length grid then None
   else
-    (* Printf.printf "Searching upwards from row: %s idx %d\n" row row_idx; *)
-    List.for_all
-      (fun offset ->
-        fetch_grid_elem grid (row_idx - offset) idx
-        = String.get search_string offset)
-      search_idxs
+    let row_val = Array.get grid row in
+    if col < 0 || col >= String.length row_val then None
+    else Some (String.get row_val col)
 
-let search_occurrence_downward search_string grid row idx =
-  let row_idx = grid |> Array.find_index (fun l -> l = row) |> Option.get in
-  let search_length = String.length search_string - 1 in
-  let search_idxs = List.init (String.length search_string) (fun i -> i) in
-  if row_idx + search_length >= Array.length grid then false
-  else
-    (* Printf.printf "Searching downwards from row: %s idx %d\n" row row_idx; *)
-    List.for_all
-      (fun offset ->
-        fetch_grid_elem grid (row_idx + offset) idx
-        = String.get search_string offset)
-      search_idxs
+let rec match_direction grid word row col dx dy =
+  match word with
+  | [] -> true
+  | x :: xs -> (
+      match grid_get_opt grid row col with
+      | Some e when e = x -> match_direction grid xs (row + dx) (col + dy) dx dy
+      | _ -> false)
 
-let search_occurrence_left_diagonal_upward search_string grid row_idx col_idx =
-  let search_length = String.length search_string - 1 in
-  let search_idxs = List.init (String.length search_string) (fun i -> i) in
-  if row_idx - search_length < 0 || col_idx - search_length < 0 then false
-  else
-    List.for_all
-      (fun offset ->
-        fetch_grid_elem grid (row_idx - offset) (col_idx - offset)
-        = String.get search_string offset)
-      search_idxs
+let search_directions grid word row col directions =
+  let word_list = word |> String.to_seq |> List.of_seq in
+  directions
+  |> List.map (fun (dx, dy) ->
+         if match_direction grid word_list row col dx dy then 1 else 0)
+  |> List.fold_left ( + ) 0
 
-let search_occurrence_left_diagonal_downward search_string grid row_idx col_idx
-    =
-  let search_length = String.length search_string - 1 in
-  let search_idxs = List.init (String.length search_string) (fun i -> i) in
-  let grid_len = Array.length grid in
-  if row_idx + search_length >= grid_len || col_idx - search_length < 0 then
-    false
-  else
-    List.for_all
-      (fun offset ->
-        fetch_grid_elem grid (row_idx + offset) (col_idx - offset)
-        = String.get search_string offset)
-      search_idxs
+let count_row_occurrences grid word directions row =
+  Array.get grid row |> String.to_seq |> List.of_seq
+  |> List.mapi (fun col _ -> search_directions grid word row col directions)
+  |> List.fold_left ( + ) 0
 
-let search_occurrence_right_diagonal_upward search_string grid row_idx col_idx =
-  let search_length = String.length search_string - 1 in
-  let search_idxs = List.init (String.length search_string) (fun i -> i) in
-  let row_len = row_idx |> Array.get grid |> String.length in
-  if row_idx - search_length < 0 || col_idx + search_length >= row_len then
-    false
-  else
-    List.for_all
-      (fun offset ->
-        fetch_grid_elem grid (row_idx - offset) (col_idx + offset)
-        = String.get search_string offset)
-      search_idxs
-
-let search_occurrence_right_diagonal_downward search_string grid row_idx col_idx
-    =
-  let search_length = String.length search_string - 1 in
-  let search_idxs = List.init (String.length search_string) (fun i -> i) in
-  let grid_len = Array.length grid in
-  let row_len = row_idx |> Array.get grid |> String.length in
-  if row_idx + search_length >= grid_len || col_idx + search_length >= row_len
-  then false
-  else
-    List.for_all
-      (fun offset ->
-        fetch_grid_elem grid (row_idx + offset) (col_idx + offset)
-        = String.get search_string offset)
-      search_idxs
-
-let count_occurrences_from_pos search_string grid row_idx col_idx =
-  let row = Array.get grid row_idx in
-  [
-    search_occurrence_forward search_string row col_idx;
-    search_occurrence_backward search_string row col_idx;
-    search_occurrence_upward search_string grid row col_idx;
-    search_occurrence_downward search_string grid row col_idx;
-    search_occurrence_left_diagonal_upward search_string grid row_idx col_idx;
-    search_occurrence_left_diagonal_downward search_string grid row_idx col_idx;
-    search_occurrence_right_diagonal_upward search_string grid row_idx col_idx;
-    search_occurrence_right_diagonal_downward search_string grid row_idx col_idx;
-  ]
-  |> List.filter (fun x -> x = true)
-  |> List.length
-
-let count_occurrences_from_row search_string grid row_idx =
-  let row = Array.get grid row_idx in
-  let first_letter = String.get search_string 0 |> Char.uppercase_ascii in
-  let rec loop acc idx =
-    if idx >= String.length row then acc
-    else if String.get row idx <> first_letter then loop acc (idx + 1)
-    else
-      loop
-        (acc + count_occurrences_from_pos search_string grid row_idx idx)
-        (idx + 1)
-  in
-  loop 0 0
-
-let count_occurrences_grid search_string grid =
-  let first_letter = String.get search_string 0 |> Char.uppercase_ascii in
+let count_occurrences grid word directions =
+  let first_letter = String.get word 0 in
   Array.mapi
     (fun idx row ->
       if not (String.contains row first_letter) then 0
-      else
-        let c = count_occurrences_from_row search_string grid idx in
-        c)
+      else count_row_occurrences grid word directions idx)
     grid
   |> Array.fold_left ( + ) 0
 
-let count_occurrences string file =
+let search_word word file =
   let data = Utils.read_lines file in
   let grid = Array.of_list data in
-  let search_string = string in
-  count_occurrences_grid search_string grid
+  count_occurrences grid word directions
 
 let run () =
-  let search_string = "XMAS" in
-  let count = count_occurrences search_string "inputs/day04.txt" in
-  Printf.printf "# of occurrences of %s: %d\n" search_string count
+  let word = "XMAS" in
+  let count = search_word word "inputs/day04.txt" in
+  Printf.printf "# of occurrences of %s: %d\n" word count
 
 let run_tests () =
   List.iter
-    (fun (search_string, file, expected_count) ->
-      let actual_count = count_occurrences search_string file in
+    (fun (word, file, expected_count) ->
+      let actual_count = search_word word file in
       Utils.assert_equal ~expected:expected_count ~actual:actual_count)
     [ ("XMAS", "inputs/day04ex.txt", 18); ("XMAS", "inputs/day04.txt", 2593) ]
